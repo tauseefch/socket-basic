24e06c548fb3cce1c0cc86f4bd739e5172d2363e
Tauseef <Muneeb.me@gmail.com>
2016-10-21 09:46:54 +0500

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,8 @@ var socket = io();
 socket.on('connect', function() {
 	console.log('Connected to Socket.io server!');
 });
+
+socket.on('message', function(message) {
+	console.log('New Message:');
+	console.log(message.text);
+});
