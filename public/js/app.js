aa7e83943d8112b1294dc39fca4b89b4ffd72f6c
Tauseef <Muneeb.me@gmail.com>
2016-10-24 08:01:42 +0500

    Add support for chat room

--- public/js/app.js
+++ public/js/app.js
@@ -5,13 +5,20 @@ var socket = io();
 
 socket.on('connect', function() {
 	console.log('Connected to Socket.io server!');
+
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	})
 });
 
 socket.on('message', function(message) {
 	var momentTimestamp = moment.utc(message.timestamp);
 	var $message = $('.messages');
+	var $roomname = $('.room-title');
 	console.log('New Message:');
 	console.log(message.text);
+	$roomname.text(room);
 	$message.append("<p><strong>" + message.name + " " + momentTimestamp.local().format('h:mm a') + "</strong></p>");
 	$message.append("<p>" + message.text + "</p>");
 });
